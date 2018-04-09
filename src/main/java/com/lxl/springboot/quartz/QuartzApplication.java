d15ace7680d511b8d35e55fb2adc4b878d2d2cca
luxinglin <651150151@qq.com>
2018-04-09 15:18:43 +0800

    refact package

--- src/main/java/com/lxl/springboot/quartz/QuartzApplication.java
+++ src/main/java/com/lxl/springboot/quartz/QuartzApplication.java
@@ -11,8 +11,8 @@ import org.springframework.context.annotation.ComponentScan;
  * @Description: TODO
  * @create 2018-04-09 10:48
  **/
-@ComponentScan("com.lxl.quartz")
-@MapperScan("com.lxl.quartz.dao")
+@ComponentScan("com.lxl.springboot.quartz")
+@MapperScan("com.lxl.springboot.quartz.dao")
 @SpringBootApplication
 public class QuartzApplication {
 
